712f6fb49d8334a630c06f8c209e0443479474d3
Sean Sullivan <dev@seanmedia.ca>
2020-05-14 18:30:21 -0600

    Add components

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,8 +1,91 @@
-import React from "react";
+import React, { useState } from "react";
+
+import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 import "components/Application.scss";
 
+const days = [
+  {
+    id: 1,
+    name: "Monday",
+    spots: 2,
+  },
+  {
+    id: 2,
+    name: "Tuesday",
+    spots: 5,
+  },
+  {
+    id: 3,
+    name: "Wednesday",
+    spots: 0,
+  },
+];
+
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  },
+  {
+    id: 6,
+    time: "5pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  }
+];
+
+
 export default function Application(props) {
+  const [ day, setDay ] = useState("Monday");
+  
   return (
     <main className="layout">
       <section className="sidebar">
@@ -12,7 +95,13 @@ export default function Application(props) {
         alt="Interview Scheduler"
       />
       <hr className="sidebar__separator sidebar--centered" />
-      <nav className="sidebar__menu"></nav>
+      <nav className="sidebar__menu">
+        <DayList 
+          days={ days }
+          day={ day }
+          setDay={ setDay }
+        />
+      </nav>
       <img
         className="sidebar__lhl sidebar--centered"
         src="images/lhl.png"
@@ -20,7 +109,8 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => <Appointment key={appointment.id} {...appointment} />)}
+        {<Appointment key="last" time="6pm" />}
       </section>
     </main>
   );
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,11 +1,27 @@
 import React from "react";
+import classNames from "classnames";
+
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+
+  const formatSpots = spots => {
+    return `${spots ? spots : 'no'} spot${spots > 1 || spots === 0 ? 's' : ''} remaining`;
+  }
+
   const { name, spots, selected, setDay } = props;
+  const dayClass = classNames({
+    "day-list__item": true,
+    "day-list__item--selected": selected,
+    "day-list__item--full": spots === 0
+ });
   return (
-    <li onClick={ () => setDay(name) }>
+    <li
+      className={ dayClass }
+      onClick={ () => setDay(name) }
+    >
       <h2 className="text--regular">{ name }</h2> 
-      <h3 className="text--light">{ spots }</h3>
+      <h3 className="text--light">{ formatSpots(spots) }</h3>
     </li>
   );
 }
