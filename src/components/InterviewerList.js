69875449fc4bd8f50df88ede386e15dba671aaf6
Sean Sullivan <dev@seanmedia.ca>
2020-05-18 14:16:25 -0600

    Test Appointment component

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from 'prop-types';
 
 import InterviewerListItem from "components/InterviewerListItem";
 
@@ -23,4 +24,9 @@ export default function InterviewerList(props) {
       </ul>
     </section>
   );
+}
+
+InterviewerList.propTypes = {
+  value: PropTypes.number,
+  onChange: PropTypes.func.isRequired
 }
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,38 +1,36 @@
-import { useReducer, useEffect } from "react";
+import { useState, useReducer, useEffect } from "react";
 import axios from "axios";
 
 const SET_DAY = "SET_DAY";
 const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
-const SET_INTERVIEW = "SET_INTERVIEW"
+const SET_INTERVIEW = "SET_INTERVIEW";
+const SET_REMAINING_SPOTS = "SET_REMAINING_SPOTS";
 
 function reducer(state, action) {
   if (action.type === SET_DAY) {
-    return {
-      ...state,
-      day: action.day
-    };
+    const { day } = action;
+    return { ...state, day };
   }
+
   if (action.type === SET_APPLICATION_DATA) {
+    const { days, appointments, interviewers } = action;
+    return { ...state, days, appointments, interviewers };
+  }
+
+  if (action.type === SET_INTERVIEW) {
+    const { id, interview } = action;
+    const appointment = { ...state.appointments[id], interview };
     return {
       ...state,
-      days: action.days,
-      appointments: action.appointments,
-      interviewers: action.interviewers
+      appointments: { ...state.appointments, [id]: appointment },
+      interviewers: { ...state.interviewers }
     };
   }
-  if (action.type === SET_INTERVIEW) {
-    const appointment = {
-      ...state.appointments[action.id],
-      interview: action.interview || null
-    };
-    const appointments = {
-      ...state.appointments,
-      [action.id]: appointment
-    };
-    return {
-      ...state,
-      appointments
-    };
+
+  if (action.type === SET_REMAINING_SPOTS) {
+    return { ...state, days: state.days.map(day => {
+      return { ...day, spots: day.spots + action.value }
+    })};
   }
 }
 
@@ -52,55 +50,33 @@ export function useApplicationData() {
       axios.get('/api/appointments'),
       axios.get('/api/interviewers')
     ]).then((all) => {
-      dispatch({
-        type: SET_APPLICATION_DATA,
-        days: all[0].data,
-        appointments: all[1].data,
-        interviewers: all[2].data
-      })
+      const [ days, appointments, interviewers ] = all.map(res => res.data);
+      dispatch({ type: SET_APPLICATION_DATA, days, appointments, interviewers });
     })
   }, []);
 
-  // state.days.find((day) => day.name === state.day)
-
   function bookInterview(id, interview) {
-    const days = [ ...state.days];
-    const currentDay = days.find((day) => day.name === state.day);
-    currentDay.spots--;
-
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    
     return axios({
       method: "put",
       url: `/api/appointments/${id}`,
-      data: appointment
+      data: { ...state.appointments[id], interview: { ...interview } }
     }).then((response) => {
-        dispatch({ type: SET_INTERVIEW, interview });
-        return response;
-      });
+      dispatch({ type: SET_INTERVIEW, id, interview });
+      dispatch({ type: SET_REMAINING_SPOTS, value: -1 });
+      return response;
+    });
   }
 
   function cancelInterview(id) {
-    const days = [ ...state.days];
-    const currentDay = days.find((day) => day.name === state.day);
-    currentDay.spots++;
-
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    };
-    
     return axios({
       method: "delete",
       url: `/api/appointments/${id}`,
-      data: appointment
+      data: { ...state.appointments[id], interview: null }
     }).then((response) => {
-        dispatch({ type: SET_INTERVIEW, interview: null });
-        return response;
-      });
+      dispatch({ type: SET_INTERVIEW, id, interview: null });
+      dispatch({ type: SET_REMAINING_SPOTS, value: 1 });
+      return response;
+    });
   }
 
   return { state, setDay, bookInterview, cancelInterview }
