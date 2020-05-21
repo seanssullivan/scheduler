97d45050668468885e38217376dbb21846db8038
Sean Sullivan <dev@seanmedia.ca>
2020-05-20 18:21:59 -0600

    Add Cypress tests for appointments

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -36,18 +36,49 @@ describe("Appointments", () => {
   it("should edit an interview", () => {
 
     // Clicks the edit button for the existing appointment
+    cy.get("[alt=Edit]")
+      .first()
+      .click({force: true});
+
     // Changes the name and interviewer
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .should('have.value', '')
+      .type("Lydia Miller-Jones");
+
+    cy.get("[alt='Tori Malcolm']")
+      .click();
+
     // Clicks the save button
+    cy.contains("Save")
+      .click();
+
     // Sees the edit to the appointment
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
 
   });
 
   it("should cancel an interview", () => {
 
     // Clicks the delete button for the existing appointment
-    // Clicks the confirm button
-    // Sees that the appointment slot is empty
+    cy.get("[alt=Delete]")
+      .first()
+      .click({force: true});
 
+    // Clicks the confirm button
+    cy.contains("Confirm")
+      .click();
+
+    // Sees that the appointment slot is empty
+    cy.contains("Deleting...")
+      .should('exist');
+
+    cy.contains("Deleting...")
+      .should('not.exist');
+
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should('not.exist');;
   });
 
 });
